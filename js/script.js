ecc3dfbc47e3ff6c522f6fa973f5b9aacce8a7ed
KALO SOUALIO <kalosoualio6@gmail.com>
2025-01-27 19:44:07 +0000

    Ajout de la fonction random

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const colorBox = document.getElementById("color-box");
   const colorBtn = document.getElementById("change-color-btn");
 
+  function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+  }
+
   function getRandomColor() {
     let rndCol =
-      "rgb(" +
-      Math.random(255) +
-      "," +
-      Math.random(255) +
-      "," +
-      Math.random(255) +
-      ")";
+      "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
     colorBox.style.backgroundColor = rndCol;
     console.log(rndCol);
   }
